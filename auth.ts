94c7d963359983d677f7cabdec61058759b67f23
Chris Gillis <126268722+chris85gillis@users.noreply.github.com>
2024-10-28 15:26:08 -0500

    Getting started on project

--- auth.ts
+++ auth.ts
@@ -1,4 +1,6 @@
+import exp from "constants";
 import NextAuth from "next-auth";
+import GitHubProvider from "next-auth/providers/github";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   theme: {
@@ -6,11 +8,21 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     logo: "/logo.png",
     buttonText: "#ffffff",
   },
-  providers: [],
+  providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID as string,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+    }),
+  ],
   callbacks: {
+    // Session callback to pass email to session
+    async session({ session, token }) {
+      session.user.email = token.email;
+      return session;
+    },
+    // Redirect to login if unauthorized
     authorized: async ({ auth }) => {
-      // Logged in users are authenticated, otherwise redirect to login page
-      return !!auth;
+      return !!auth; // true if authenticated, false otherwise
     },
   },
 });
